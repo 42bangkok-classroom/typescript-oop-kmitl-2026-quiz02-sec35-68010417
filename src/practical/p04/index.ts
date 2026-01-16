297702ec834f4d9d518e1117c094bdab0d31d1a7
68010417 <68010417@kmitl.ac.th>
2026-01-16 10:56:30 +0700

    p04_1

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,1 +1,32 @@
-export function countCommentsByPost() {}
+import axios from 'axios';
+
+interface Comment {
+  postId: number;
+  id: number;
+}
+
+type CommentCountByPost = Record<number, number>;
+
+export async function countCommentsByPost(): Promise<CommentCountByPost> {
+  try {
+    const response = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments');
+    
+    const comments = response.data;
+
+    const commentCount = comments.reduce((acc, comment) => {
+      if (comment.postId === null || comment.postId === undefined) {
+        return acc;
+      }
+      if (!acc[comment.postId]) {
+        acc[comment.postId] = 0;
+      }
+      acc[comment.postId]++;
+      
+      return acc;
+    }, {} as CommentCountByPost);
+
+    return commentCount;
+  } catch (error) {
+    throw new Error('error');
+  }
+}
