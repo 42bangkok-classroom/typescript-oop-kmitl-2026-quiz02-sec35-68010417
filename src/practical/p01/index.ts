a182b34c49d1b6459fd25b28d11d967aa1b399fb
68010417 <68010417@kmitl.ac.th>
2026-01-16 10:02:24 +0700

    p01_1

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,29 @@
-export function getEdgePosts() {}
+import axios from "axios";
+
+interface Posts{
+  id:number;
+  title:string;
+}
+
+interface EdgePosts{
+  id:number;
+  title:string;
+}
+
+export async function getEdgePosts():Promise<EdgePosts[]> {
+try{
+  const get = await axios.get<Posts[]>(`https://jsonplaceholder.typicode.com/posts`);
+  const result = get.data;
+
+  const firstpost = result[0]
+  const lastpost = result[result.length-1]
+
+  return [
+    {id: firstpost.id,title: firstpost.title},
+    {id: lastpost.id,title: lastpost.title}
+  ]
+
+}catch(error){
+  throw new Error("error");
+}
+}
